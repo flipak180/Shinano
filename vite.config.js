ef7986bf52732efea0d7fc4be28044550c95448d
flipak180@mail.ru <flipak180@mail.ru>
2023-03-03 18:32:53 +0300

    catalog.html contacts.html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import posthtml from '@vituum/vite-plugin-posthtml'
+import {resolve} from 'path'
 
 export default {
     root: 'src',
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                index: resolve('src/index.html'),
+                catalog: resolve('src/catalog.html'),
+                contacts: resolve('src/contacts.html'),
+            },
+        },
     },
     plugins: [
         posthtml({
