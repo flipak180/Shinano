0c75a5486c38b035c150b1a96cd4dfcd933f6c87
flipak180@mail.ru <flipak180@mail.ru>
2023-03-25 01:06:35 +0300

    profile.html

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default {
                 purchase: resolve('src/purchase.html'),
                 success: resolve('src/success.html'),
                 inner: resolve('src/inner.html'),
+                profile: resolve('src/profile.html'),
             },
         },
     },
